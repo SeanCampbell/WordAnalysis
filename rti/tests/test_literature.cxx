42434df5015fc76293547083e24b6604144e5058
ngallagher1 <ngallagher1@gaels.iona.edu>
2016-02-23 11:30:14 -0500

    Update test_literature.cxx

--- rti/tests/test_literature.cxx
+++ rti/tests/test_literature.cxx
@@ -10,21 +10,6 @@ namespace
 
   static void test_all()
   {
-    rti_book_sptr book = new rti_book("First Book","Charlene Tsai", PK, "input_file.txt");
-    rti_literature_sptr lit= new rti_literature();
-    int index;
-    if (!lit->find(book->title(),index)) lit->insert(book, index);
-
-    rti_book_sptr book2 = new rti_book("cat","Marlene Zakierski", PK, "input_file.txt");
-    if (!lit->find(book2->title(),index)) lit->insert(book2, index);
-
-    TEST("Test literature Insertion: \n", (*lit)[0]->title()=="cat", true);
-    TEST("Test literature Insertion: \n", (*lit)[1]->title()=="First Book", true);
-    vcl_cout<<"number of books = "<<lit->size()<<vcl_endl;
-    TEST("Test literature xml write: \n", lit->write_xml("literature.xml")==XML_SUCCESS, true); 
-    rti_literature_sptr lit2= new rti_literature();
-    TEST("Test literature xml read: \n", lit2->read_xml("literature.xml")==XML_SUCCESS, true);
-    TEST("Test literature xml write: \n", lit2->write_xml("literature.xml")==XML_SUCCESS, true);
   }
 
 }
